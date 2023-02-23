1802b815b65aae6dceacc84aec8af3d82f84a332
Darren Oliveiro-Priestnall <darren.op@jeskei.com>
2023-02-23 11:59:14 +0000

    moved reserve ratio logic from base up to the reserve ratio controller

--- contracts/ReserveRatioController.sol
+++ contracts/ReserveRatioController.sol
@@ -6,7 +6,17 @@ import "./base/BaseController.sol";
 
 contract ReserveRatioController is BaseController {
 
-    constructor(uint _defaultMinRatio) BaseController(_defaultMinRatio) {}
+    // Minimum reserve ratio
+    uint defaultMinRatio;
+    
+    constructor(uint _defaultMinRatio) BaseController()
+    {
+        defaultMinRatio = _defaultMinRatio;
+    }
+
+    // Managed Pools
+    mapping(address => uint) public reserveRatio; // MP Address and its Reserve Ratio
+
 
     // Checks if the ratio is breached and transfers as necessary
     function runCheck(address _poolToCheck) public view restricted returns(uint)
@@ -16,5 +26,13 @@ contract ReserveRatioController is BaseController {
         // Calculate reserve requirement as deposits * rr
         // If this is higher than reserve, transfer difference
         return 1;
+    }
+
+    function setReserveRatio(address _poolAddress, uint _ratio) public restricted {
+        reserveRatio[_poolAddress] = _ratio;
+    }
+
+    function getManagedPoolRatio(address _poolAddress) public view returns (uint) {
+        return reserveRatio[_poolAddress];
     }
 }
--- contracts/base/BaseController.sol
+++ contracts/base/BaseController.sol
@@ -9,31 +9,16 @@ interface IController {
 abstract contract BaseController is IController {
     address public manager;
 
-    // Managed Pools
-    mapping(address => uint) public reserveRatio; // MP Address and its Reserve Ratio
-
-    // Minimum reserve ratio
-    uint defaultMinRatio;
-
-    constructor(uint _defaultMinRatio) {
-        defaultMinRatio = _defaultMinRatio;
+    constructor() {
         manager = msg.sender;
     }
     
     function transferManagement(address _newManager) public restricted {
         manager = _newManager;
     }
-
-    function setReserveRatio(address _poolAddress, uint _ratio) public restricted {
-        reserveRatio[_poolAddress] = _ratio;
-    }
     
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
-    function getManagedPoolRatio(address _poolAddress) public view returns (uint) {
-        return reserveRatio[_poolAddress];
-    }
-}   
+}
